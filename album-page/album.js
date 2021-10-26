4cfa21ec1143d68b253365141275689b33c51978
yonatanski <phpcodeyg@gmail.com>
2021-10-26 15:39:38 +0200

    FIRST COMMIT

---
+++ album-page/album.js
@@ -0,0 +1,18 @@
+let visibility = true;
+const navVisibility = () => {
+  let nav = document.querySelector("nav");
+  let main = document.querySelector("main");
+
+  visibility ? nav.classList.add("d-none") : nav.classList.remove("d-none");
+  visibility ? main.classList.remove("d-none") : main.classList.add("d-none");
+
+  visibility = !visibility;
+};
+
+let menuButtonMain = document
+  .getElementById("menuButton")
+  .addEventListener("click", () => navVisibility());
+
+let menuButtonNav = document
+  .getElementById("menuButtonNav")
+  .addEventListener("click", () => navVisibility());
